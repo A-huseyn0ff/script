acc3a75578bf3c7f5f08bfe85089cd4c382f8547
A-huseyn0ff <tu7ej3brm@code.edu.az>
2023-10-11 19:31:01 -0700

    js code added!

---
+++ script.js
@@ -0,0 +1,91 @@
+//1den 100e qeder ededler icerisinde 5e ve 3e bolunenlerin toplamin tapan algoritm
+
+//let sum=0
+//for (let i = 1; i <= 100; i++) {
+//if (i%5==0 || i%3==0) {
+// sum+=i
+//}
+//}
+//console.log(sum);
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Verilen ededin 2nin quvveti olub olmadigini bildiren algoritm
+
+//let number = 31
+//if (number <0) {
+ // console.log("2-nin quvvəti deyil.");
+//} 
+//else {
+  //while (number > 1 && number % 2 === 0) {
+  //  number /= 2;
+  //}
+ // if (number === 1) {
+  //  console.log(" 2-nin quvvətidir.");
+//} 
+//else {
+   // console.log(" 2-nin quvvəti deyil.");
+//}
+//}
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Verilen ededin sade ve murekkeb oldugunu bildiren algoritm
+//let a=1
+//let sade=true
+//if (a===1 && a===2) {
+//  sade=false;
+ //console.log('ne sade,ne de murekkeb ededdir');
+//}
+//else if (a>2){
+ // for (let i = 2; i < a; i++){
+ //if (a%i==0) {
+ // sade=false;
+  //break;
+ //}
+//}
+//if (sade) {
+ // console.log('bu sadedir');
+//}
+//else{
+ // console.log('bu murekkebdir');
+//}
+
+//}
+//else{
+//  console.log('ne sade ne murekkebdir');
+//}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Gelen aylara gore hansi fesil oldugunu bildiren algoritm.Deger Consoledan girilecek.Sabit deger olmayacaq.
+
+//const ay=prompt('ayi daxil edin')
+//switch (ay) {
+//case 'sentyabr':
+// case 'oktyabr':
+//   case 'noyabr':
+//console.log('payiz fesli');
+// break;
+
+//case 'dekabr':
+//case 'yanvar':
+//  case 'fevral':
+//console.log('qis fesli');
+// break;
+
+//case 'mart':
+//  case 'aprel':
+// case 'may':
+//console.log('yaz fesli');
+//  break;
+
+// case 'iyun':
+//  case 'iyul':
+//    case 'avqust':
+//console.log('yay fesli');
+//     break;
+// default:
+//   console.log('error 404 not found');
+//}
